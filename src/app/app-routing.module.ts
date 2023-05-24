dc414415d7dd7f80fb0c93ddd2a4dfdcf090e77b
shanker.salil@tcs.com <shanker.salil.ap@nielseniq.com>
2023-05-25 00:34:17 +0530

    added routerLink

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,11 +1,16 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router'; // CLI imports router
 import { MediaItemListComponent } from './components/media-item-list/media-item-list.component';
+import { InputMediaItemComponent } from './components/input-media-item/input-media-item.component';
 
 const routes: Routes = [
     {
         path: 'media',
         component: MediaItemListComponent
+    },
+    {
+        path: 'add',
+        component: InputMediaItemComponent
     }
 ]; // sets up routes constant where you define your routes
 
