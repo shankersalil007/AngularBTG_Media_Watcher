e79c121b37f4d29b9b481c8f89ced5a11d7ba354
shanker.salil@tcs.com <shanker.salil.ap@nielseniq.com>
2023-05-24 20:45:22 +0530

    added ng class for applying conditional class

--- src/app/components/media-item-list/media-item-list.component.ts
+++ src/app/components/media-item-list/media-item-list.component.ts
@@ -19,21 +19,21 @@ export class MediaItemListComponent implements OnInit {
       name: 'The Redemption',
       year: 1684928725540,
       category: 'Action',
-      isFavorite: false,
+      isFavorite: true,
     },
     {
       id: 2,
       name: 'The Dark Knight',
       year: 1211659951000,
       category: 'Action',
-      isFavorite: true,
+      isFavorite: false,
     },
     {
       id: 3,
       name: 'The Prestige',
       year: 1148501551000,
       category: 'Drama',
-      isFavorite: true,
+      isFavorite: false,
     }
 
   ]
